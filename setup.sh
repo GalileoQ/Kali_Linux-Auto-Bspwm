4ee4bdf0fe32759b4f827f5ffa5374cc59054af4
GalileoQ <141581409+GalileoQ@users.noreply.github.com>
2025-07-07 23:35:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -46,7 +46,7 @@ else
 	sleep 1
 	echo -e "\n\n${blueColour}[*] Installing necessary packages for the environment...\n${endColour}"
 	sleep 2
-	sudo apt install -y kitty rofi feh xclip ranger i3lock-fancy scrot scrub wmname imagemagick cmatrix htop btop fastfetch python3-pip procps tty-clock fzf lsd bat pamixer flameshot glances 
+	sudo apt install -y kitty rofi feh xclip ranger i3lock-fancy scrot scrub wmname imagemagick cmatrix htop hollywood btop fastfetch python3-pip procps tty-clock fzf lsd bat pamixer flameshot glances 
 	if [ $? != 0 ] && [ $? != 130 ]; then
 		echo -e "\n${redColour}[-] Failed to install some packages!\n${endColour}"
 		exit 1
